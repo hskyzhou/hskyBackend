822784311c1070cea9be49354cd510719aa1aa65
hsky <wen.zhou@bioon.com>
2016-10-24 16:55:04 +0800

    增加权限配置文件

---
+++ config/backend/permission.php
@@ -0,0 +1,12 @@
+<?php 
+	return [
+		'permissions' => [
+			'manage' => 'permission.manage',
+			'create' => 'permission.create',
+			'update' => 'permission.update',
+			'delete' => 'permission.delete',
+			'search' => [
+				'manage' => 'permission.search.manage',
+			],
+		]
+	];
