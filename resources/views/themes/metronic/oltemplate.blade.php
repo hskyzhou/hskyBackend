b4ac0cee61870252350661654bcccdc6493fdecf
hsky <wen.zhou@bioon.com>
2016-11-17 18:08:32 +0800

    修改模板

---
+++ resources/views/themes/metronic/oltemplate.blade.php
@@ -0,0 +1,167 @@
+@inject('presenter', 'App\ViewPresenters\Backend\PermissionViewPresenter')
+@extends('themes.metronic.common.layout')
+
+@section('css')
+	{{-- <link href="{{asset('themes/metronic/global/plugins/domenu/jquery.domenu-0.95.77.css')}}" rel="stylesheet" type="text/css" /> --}}
+	<link href="{{asset('themes/metronic/global/plugins/jquery-nestable/jquery.nestable.css')}}" rel="stylesheet" type="text/css" />
+	
+	<style type="text/css">
+		
+		.content-class{
+			cursor : pointer;
+		}
+	</style>
+@endsection
+
+@section('content')
+	<h3 class="page-title"> Ajax Datatables
+	    <small>basic datatable samples</small>
+	</h3>
+	<!-- END PAGE TITLE-->
+	<!-- END PAGE HEADER-->
+	<div class="row">
+	    <div class="col-md-12">
+	        <div class="note note-danger">
+	            <p> NOTE: The below datatable is not connected to a real database so the filter and sorting is just simulated for demo purposes only. </p>
+	        </div>
+	        <!-- Begin: life time stats -->
+	        <div class="portlet light portlet-fit portlet-datatable bordered">
+	            <div class="portlet-title">
+	                <div class="caption">
+	                    <i class="icon-settings font-dark"></i>
+	                    <span class="caption-subject font-dark sbold uppercase">Ajax Datatable</span>
+	                </div>
+	                <div class="actions">
+	                    <div class="btn-group btn-group-devided" data-toggle="buttons">
+	                        <label class="btn btn-transparent grey-salsa btn-outline btn-circle btn-sm active">
+	                            <input type="radio" name="options" class="toggle" id="option1">Actions</label>
+	                        <label class="btn btn-transparent grey-salsa btn-outline btn-circle btn-sm">
+	                            <input type="radio" name="options" class="toggle" id="option2">Settings</label>
+	                    </div>
+	                    <div class="btn-group">
+	                        <a class="btn red btn-outline btn-circle" href="javascript:;" data-toggle="dropdown">
+	                            <i class="fa fa-share"></i>
+	                            <span class="hidden-xs"> Tools </span>
+	                            <i class="fa fa-angle-down"></i>
+	                        </a>
+	                        <ul class="dropdown-menu pull-right">
+	                            <li>
+	                                <a href="javascript:;"> Export to Excel </a>
+	                            </li>
+	                            <li>
+	                                <a href="javascript:;"> Export to CSV </a>
+	                            </li>
+	                            <li>
+	                                <a href="javascript:;"> Export to XML </a>
+	                            </li>
+	                            <li class="divider"> </li>
+	                            <li>
+	                                <a href="javascript:;"> Print Invoices </a>
+	                            </li>
+	                        </ul>
+	                    </div>
+	                </div>
+	            </div>
+	            <div class="portlet-body">
+	                <div class="table-container">
+	                    <div class="table-actions-wrapper">
+	                        <span> </span>
+	                        <select class="table-group-action-input form-control input-inline input-small input-sm">
+	                            <option value="">Select...</option>
+	                            <option value="Cancel">Cancel</option>
+	                            <option value="Cancel">Hold</option>
+	                            <option value="Cancel">On Hold</option>
+	                            <option value="Close">Close</option>
+	                        </select>
+	                        <button class="btn btn-sm green table-group-action-submit">
+	                            <i class="fa fa-check"></i> Submit</button>
+	                    </div>
+	                    <table class="table table-striped table-bordered table-hover table-checkable" id="datatable_ajax">
+	                        <thead>
+	                            <tr role="row" class="heading">
+	                                <th width="2%">
+	                                    <input type="checkbox" class="group-checkable"> </th>
+	                                <th width="5%"> Record&nbsp;# </th>
+	                                <th width="15%"> Date </th>
+	                                <th width="200"> Customer </th>
+	                                <th width="10%"> Ship&nbsp;To </th>
+	                                <th width="10%"> Price </th>
+	                                <th width="10%"> Amount </th>
+	                                <th width="10%"> Status </th>
+	                                <th width="10%"> Actions </th>
+	                            </tr>
+	                            <tr role="row" class="filter">
+	                                <td> </td>
+	                                <td>
+	                                    <input type="text" class="form-control form-filter input-sm" name="order_id"> </td>
+	                                <td>
+	                                    <div class="input-group date date-picker margin-bottom-5" data-date-format="dd/mm/yyyy">
+	                                        <input type="text" class="form-control form-filter input-sm" readonly name="order_date_from" placeholder="From">
+	                                        <span class="input-group-btn">
+	                                            <button class="btn btn-sm default" type="button">
+	                                                <i class="fa fa-calendar"></i>
+	                                            </button>
+	                                        </span>
+	                                    </div>
+	                                    <div class="input-group date date-picker" data-date-format="dd/mm/yyyy">
+	                                        <input type="text" class="form-control form-filter input-sm" readonly name="order_date_to" placeholder="To">
+	                                        <span class="input-group-btn">
+	                                            <button class="btn btn-sm default" type="button">
+	                                                <i class="fa fa-calendar"></i>
+	                                            </button>
+	                                        </span>
+	                                    </div>
+	                                </td>
+	                                <td>
+	                                    <input type="text" class="form-control form-filter input-sm" name="order_customer_name"> </td>
+	                                <td>
+	                                    <input type="text" class="form-control form-filter input-sm" name="order_ship_to"> </td>
+	                                <td>
+	                                    <div class="margin-bottom-5">
+	                                        <input type="text" class="form-control form-filter input-sm" name="order_price_from" placeholder="From" /> </div>
+	                                    <input type="text" class="form-control form-filter input-sm" name="order_price_to" placeholder="To" /> </td>
+	                                <td>
+	                                    <div class="margin-bottom-5">
+	                                        <input type="text" class="form-control form-filter input-sm margin-bottom-5 clearfix" name="order_quantity_from" placeholder="From" /> </div>
+	                                    <input type="text" class="form-control form-filter input-sm" name="order_quantity_to" placeholder="To" /> </td>
+	                                <td>
+	                                    <select name="order_status" class="form-control form-filter input-sm">
+	                                        <option value="">Select...</option>
+	                                        <option value="pending">Pending</option>
+	                                        <option value="closed">Closed</option>
+	                                        <option value="hold">On Hold</option>
+	                                        <option value="fraud">Fraud</option>
+	                                    </select>
+	                                </td>
+	                                <td>
+	                                    <div class="margin-bottom-5">
+	                                        <button class="btn btn-sm green btn-outline filter-submit margin-bottom">
+	                                            <i class="fa fa-search"></i> Search</button>
+	                                    </div>
+	                                    <button class="btn btn-sm red btn-outline filter-cancel">
+	                                        <i class="fa fa-times"></i> Reset</button>
+	                                </td>
+	                            </tr>
+	                        </thead>
+	                        <tbody> </tbody>
+	                    </table>
+	                </div>
+	            </div>
+	        </div>
+	        <!-- End: life time stats -->
+	    </div>
+	</div>
+@endsection
+
+@section('js')
+	<script src="{{asset('themes/metronic/global/plugins/jquery-nestable/jquery.nestable.js')}}" type="text/javascript"></script>
+	{{-- <script src="{{asset('themes/metronic/global/plugins/domenu/jquery.domenu-0.95.77.min.js')}}" type="text/javascript"></script> --}}
+
+	<script type="text/javascript">
+		$(document).ready(function() {
+		  $('#nestable_list_1').nestable({
+		      group: 1
+		  });
+		});
+	</script>
+@endsection
