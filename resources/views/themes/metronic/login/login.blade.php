69f5df840f28a264b956d7bbeb468a9a177b4ecd
hsky <wen.zhou@bioon.com>
2016-12-22 09:47:19 +0800

    增加语言包，增加登录错误提示

--- resources/views/themes/metronic/login/login.blade.php
+++ resources/views/themes/metronic/login/login.blade.php
@@ -49,10 +49,9 @@
             <form class="login-form" action="{{ route('login') }}" method="post">
                 {{csrf_field()}}
                 <h3 class="form-title">Login to your account</h3>
-                <div class="alert alert-danger display-hide">
-                    <button class="close" data-close="alert"></button>
-                    <span> Enter any username and password. </span>
-                </div>
+                
+                @include('themes.metronic.common.error')
+                
                 <div class="form-group">
                     <!--ie8, ie9 does not support html5 placeholder, so we just show field title for that-->
                     <label class="control-label visible-ie8 visible-ie9">Username</label>
